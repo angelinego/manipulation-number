b093908b3cac78cca8d81f8f85cbc7a803a1ff21
Angeline Go <angeline_go87@hotmail.com>
2018-01-15 14:29:58 +0700

    assignment 2

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("LEVEL 0")
+var numberA = 10
+let numberB = 099
+const numberC = 0.02
+
+console.log(numberA)
+console.log(numberB)
+console.log(numberC)
+console.log("-----------------------------------------------------------------------");
+
+console.log("LEVEL 1")
+console.log(numberA + numberB)
+console.log(numberB - numberC)
+console.log(numberA * numberC)
+console.log(numberB / numberC)
+console.log(numberB % numberC)
+console.log("-----------------------------------------------------------------------");
+
+console.log("LEVEL 2")
+console.log("My lucky number is" + " " + numberA)
+console.log(numberA >= numberB)
+var numberD = 1000000000000
+const numberE = -3000
+console.log(numberE / numberD)
+console.log("Number" + ":" + numberE)
+console.log(numberE + "test")
